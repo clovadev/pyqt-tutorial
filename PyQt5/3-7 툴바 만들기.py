bb962858777cd016eedde66026a7c30b39e632ef
Lulin <kistssy+dev@gmail.com>
2020-11-30 11:07:34 +0900

    Create 3-7 툴바 만들기.py

---
+++ PyQt5/3-7 툴바 만들기.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5 import QtWidgets, QtGui
+
+class MyApp(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        exitAction = QtWidgets.QAction(QtGui.QIcon('../res/exit.png'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(QtWidgets.QApplication.quit)
+
+        self.statusBar()
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&File')
+        filemenu.addAction(exitAction)
+
+        self.setWindowTitle('Toolbar')
+        self.setGeometry(1000, 500, 500, 300)
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = MyApp()
+    app.exec_()
